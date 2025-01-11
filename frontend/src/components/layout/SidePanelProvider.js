8d43d5f8b5603a6992d54b03eea908835cba1650
domchao <d.chaopradith@gmail.com>
2025-01-11 13:28:00 +0000

    Update sidepanel provider to close artifacts when page changes

--- frontend/src/components/layout/SidePanelProvider.js
+++ frontend/src/components/layout/SidePanelProvider.js
@@ -1,24 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { SidePanelContext } from "../../contexts/SidePanelContext";
 import { SidePanel } from "../artifact/SidePanel";
+
+const SidePanelNavigationHandler = ({ children }) => {
+    const location = useLocation();
+    const { setOpenArtifact } = useContext(SidePanelContext);
+
+    useEffect(() => {
+        // Close any open sidepanel when the location changes
+        setOpenArtifact(null);
+    }, [location, setOpenArtifact]);
+
+    return children;
+};
 
 const SidePanelProvider = ({ children }) => {
     const [openArtifact, setOpenArtifact] = useState(null);
 
     return (
         <SidePanelContext.Provider value={{ openArtifact, setOpenArtifact }}>
-            <div
-                className={`flex h-screen ${openArtifact ? "ml-16" : "ml-0]"}`}
-            >
+            <SidePanelNavigationHandler>
                 <div
-                    className={`flex-1 overflow-auto transition-all duration-300 ease-in-out ${
-                        openArtifact ? "w-7/12" : "w-full"
+                    className={`flex h-screen ${
+                        openArtifact ? "ml-16" : "ml-0]"
                     }`}
                 >
-                    {children}
+                    <div
+                        className={`flex-1 overflow-auto transition-all duration-300 ease-in-out ${
+                            openArtifact ? "w-7/12" : "w-full"
+                        }`}
+                    >
+                        {children}
+                    </div>
+                    {openArtifact && <SidePanel artifact={openArtifact} />}
                 </div>
-                {openArtifact && <SidePanel artifact={openArtifact} />}
-            </div>
+            </SidePanelNavigationHandler>
         </SidePanelContext.Provider>
     );
 };
